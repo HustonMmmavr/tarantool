62b9b92114dc8e91da80f8b985ac6275439c6b4d
Mergen Imeev <imeevma@gmail.com>
2021-08-04 15:31:52 +0300

    sql: check number of arguments during parsing
    Prior to this patch, the ability to accept a given number of arguments
    for a function with a variable number of arguments was checked at
    runtime. After this patch, it will be checked during parsing. For
    functions with the constant number of arguments, it is always checked
    during parsing.
    
    Part of #6105

--- test/sql-tap/func5.test.lua
+++ test/sql-tap/func5.test.lua
@@ -1,6 +1,6 @@
 #!/usr/bin/env tarantool
 local test = require("sqltester")
-test:plan(29)
+test:plan(39)
 
 --!./tcltestrunner.lua
 -- 2010 August 27
@@ -276,19 +276,19 @@ test:do_catchsql_test(
     "func-5-5.1",
     [[
         SELECT LEAST(false);
-    ]], { 1, "Wrong number of arguments is passed to LEAST(): expected at least two, got 1" } )
+    ]], { 1, "Wrong number of arguments is passed to LEAST(): expected at least 2, got 1" } )
 
 test:do_catchsql_test(
     "func-5-5.2",
     [[
         SELECT GREATEST('abc');
-    ]], { 1, "Wrong number of arguments is passed to GREATEST(): expected at least two, got 1" } )
+    ]], { 1, "Wrong number of arguments is passed to GREATEST(): expected at least 2, got 1" } )
 
 test:do_catchsql_test(
     "func-5-5.3",
     [[
         SELECT LEAST();
-    ]], { 1, "Wrong number of arguments is passed to LEAST(): expected at least two, got 0" } )
+    ]], { 1, "Wrong number of arguments is passed to LEAST(): expected at least 2, got 0" } )
 
 -- Make sure that ifnull() returns type of corresponding (i.e. first
 -- non-null) argument.
@@ -368,5 +368,110 @@ test:do_execsql_test(
     ]], {
         111
     })
+
+--
+-- gh-6106: Make sure that number of arguments check is checked properly for SQL
+-- built-in functions with variable number of arguments.
+--
+
+test:do_catchsql_test(
+    "func-8.1",
+    [[
+        SELECT COUNT(1, 2);
+    ]],
+    {
+        1, "Wrong number of arguments is passed to COUNT(): expected from 0 to 1, got 2"
+    }
+)
+
+test:do_catchsql_test(
+    "func-8.2",
+    [[
+        SELECT GREATEST();
+    ]],
+    {
+        1, "Wrong number of arguments is passed to GREATEST(): expected at least 2, got 0"
+    }
+)
+
+test:do_catchsql_test(
+    "func-8.3",
+    [[
+        SELECT GROUP_CONCAT();
+    ]],
+    {
+        1, "Wrong number of arguments is passed to GROUP_CONCAT(): expected from 1 to 2, got 0"
+    }
+)
+
+test:do_catchsql_test(
+    "func-8.4",
+    [[
+        SELECT GROUP_CONCAT(1, 2, 3);
+    ]],
+    {
+        1, "Wrong number of arguments is passed to GROUP_CONCAT(): expected from 1 to 2, got 3"
+    }
+)
+
+test:do_catchsql_test(
+    "func-8.5",
+    [[
+        SELECT LEAST();
+    ]],
+    {
+        1, "Wrong number of arguments is passed to LEAST(): expected at least 2, got 0"
+    }
+)
+
+test:do_catchsql_test(
+    "func-8.6",
+    [[
+        SELECT ROUND();
+    ]],
+    {
+        1, "Wrong number of arguments is passed to ROUND(): expected from 1 to 2, got 0"
+    }
+)
+
+test:do_catchsql_test(
+    "func-8.7",
+    [[
+        SELECT ROUND(1, 2, 3);
+    ]],
+    {
+        1, "Wrong number of arguments is passed to ROUND(): expected from 1 to 2, got 3"
+    }
+)
+
+test:do_catchsql_test(
+    "func-8.8",
+    [[
+        SELECT SUBSTR('1');
+    ]],
+    {
+        1, "Wrong number of arguments is passed to SUBSTR(): expected from 2 to 3, got 1"
+    }
+)
+
+test:do_catchsql_test(
+    "func-8.9",
+    [[
+        SELECT SUBSTR('1', '2', '3', '4');
+    ]],
+    {
+        1, "Wrong number of arguments is passed to SUBSTR(): expected from 2 to 3, got 4"
+    }
+)
+
+test:do_catchsql_test(
+    "func-8.10",
+    [[
+        SELECT UUID(1, 2);
+    ]],
+    {
+        1, "Wrong number of arguments is passed to UUID(): expected from 0 to 1, got 2"
+    }
+)
 
 test:finish_test()
